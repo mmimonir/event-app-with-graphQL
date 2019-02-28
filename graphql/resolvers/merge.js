34db9696f3fa2f9f0cb13cabb94e4a6a949ca191
mmimonir <monirdhk.it@gmail.com>
2019-03-01 01:16:49 +0600

    Using Data Loader

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,6 +1,15 @@
+const DataLoader = require('dataloader');
 const Event = require('../../models/event');
 const User = require('../../models/user');
 const { dateToString } = require('../../helpers/date');
+
+const eventLoader = new DataLoader((eventIds) => {
+  return events(eventIds);
+});
+
+const userLoader = new DataLoader((userIds) => {
+  return User.find({ _id: { $in: userIds } });
+});
 
 const events = async (eventIds) => {
   try {
@@ -15,10 +24,10 @@ const events = async (eventIds) => {
 
 const user = async (userId) => {
   try {
-    const user = await User.findById(userId);
+    const user = await userLoader.load(userId.toString());
     return {
       ...user._doc,
-      createdEvents: events.bind(this, user._doc.createdEvents)
+      createdEvents: eventLoader.load.bind(this, user._doc.createdEvents)
     };
   } catch (err) {
     throw err;
@@ -27,8 +36,8 @@ const user = async (userId) => {
 
 const singleEvent = async (eventId) => {
   try {
-    const event = await Event.findById(eventId);
-    return transformEvent(event);
+    const event = await eventLoader.load(eventId.toString());
+    return event;
   } catch (err) {
     throw err;
   }
